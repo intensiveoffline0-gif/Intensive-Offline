0258143124f46a5d1f99886f02c8198631a5188f
intensiveoffline0-gif <intensiveoffline0@gmail.com>
2026-09-25 11:59:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Intensive-Offline/', // Must match your GitHub repository name exactly
+  base: '/',
 })
